146dbd3b4f654c527de9708ff40bb97abf301fea
Alvaro Olmedo <alvaro.olmedo@opensolutions.cloud>
2018-09-20 15:41:07 +0200

    Link added

--- modules/domestic/manifests/config.pp
+++ modules/domestic/manifests/config.pp
@@ -4,12 +4,22 @@ class domestic::config {
     home     => '/home/aolmedo',
     comment  => 'Alvaro Olmedo',
     password => '$1$GRs789q9$.q5zGITTZP2Rb.ZA13bFT/'
-  }->
+  }
   file { '/home/aolmedo/.tmux.conf':
     ensure  => file,
-    owner => aolmedo,
-    group => aolmedo,
-    mode   => '0600',
+    owner   => aolmedo,
+    group   => aolmedo,
+    mode    => '0600',
     content => template('domestic/tmux.conf.erb')
+    require => User['aolmedo']
   }
+  file { '/home/aolmedo/Documentos/Formacion':
+    ensure  => link,
+    owner   => aolmedo,
+    group   => aolmedo,
+    mode    => '0777',
+    target  => '/home/aolmedo/Dropbox/Formacion',
+    require => User['aolmedo']
+  }
+
 }
